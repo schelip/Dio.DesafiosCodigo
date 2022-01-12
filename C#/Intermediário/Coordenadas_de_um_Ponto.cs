3630b1c30ae4d5b88a85ec30b6ff6dd51c304f15
schelip <fcominscheffel@gmail.com>
2022-01-12 15:50:05 -0300

    adicionado desafio Coordenadas de um Ponto

---
+++ C#/Intermediário/Coordenadas_de_um_Ponto.cs
@@ -0,0 +1,37 @@
+// Leia 2 valores com uma casa decimal (x e y), que devem representar as coordenadas de um ponto em um plano. A seguir, determine qual o quadrante ao qual pertence o ponto, ou se está sobre um dos eixos cartesianos ou na origem (x = y = 0).
+
+// Se o ponto estiver na origem, escreva a mensagem “Origem”.
+
+// Se o ponto estiver sobre um dos eixos escreva “Eixo X” ou “Eixo Y”, conforme for a situação.
+
+// Entrada
+// A entrada contem as coordenadas de um ponto.
+
+// Saída
+// A saída deve apresentar o quadrante em que o ponto se encontra.
+
+using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] valores = Console.ReadLine().Split(' ');
+        double x = double.Parse(valores[0], CultureInfo.InvariantCulture);
+        double y = double.Parse(valores[1], CultureInfo.InvariantCulture);
+
+        if (x == 0.0 && y == 0.0)
+            Console.WriteLine("Origem");
+        else if (x == 0.0)
+            Console.WriteLine("Eixo Y");
+        else if (y == 0.0)
+            Console.WriteLine("Eixo X");
+        else
+            Console.WriteLine(
+                y > 0.0
+                ? (x < 0.0 ? "Q2" : "Q1")
+                : (x < 0.0 ? "Q3" : "Q4")
+            );
+    }
+}
